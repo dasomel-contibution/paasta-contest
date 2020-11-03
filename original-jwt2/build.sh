63d21c20a654f7c67baeab874d4daa902f27fee7
dasomel <dasomell@gmail.com>
2020-11-03 12:20:45 +0900

    original-jwt1,2 공통컴포넌트 JWT 적용 준비

--- original-jwt2/build.sh
+++ original-jwt2/build.sh
@@ -4,5 +4,8 @@ docker build -t localhost:5000/common-original-jwt2:2.0 .
 docker push localhost:5000/common-original-jwt2:2.0
 
 #hub.docker.com
-docker build -t dasomel/common-original-jwt12:2.0 .
+docker build -t dasomel/common-original-jwt2:2.0 .
 docker push dasomel/common-original-jwt2:2.0
+
+kubectl apply -f ./k8s/deployment.yaml
+kubectl apply -f ./k8s/service.yaml
--- original/build.sh
+++ original/build.sh
@@ -6,3 +6,7 @@ docker push localhost:5000/common-original:latest
 #hub.docker.com
 docker build -t dasomel/common-original:latest .
 docker push dasomel/common-original:latest
+
+
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
