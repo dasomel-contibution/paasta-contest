40b37e9074f71b82ad4eee901de685939b61e355
dasomel <dasomell@gmail.com>
2020-11-03 12:23:36 +0900

    공통컴포넌트 jwt적용 준비

--- original-jwt1/build.sh
+++ original-jwt1/build.sh
@@ -6,3 +6,6 @@ docker push localhost:5000/common-original-jwt1:1.0
 #hub.docker.com
 docker build -t dasomel/common-original-jwt1:1.0 .
 docker push dasomel/common-original-jwt1:1.0
+
+kubectl apply -f ./k8s/deployment.yaml
+kubectl apply -f ./k8s/service.yaml
